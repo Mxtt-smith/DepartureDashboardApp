31d299c0b171870dbcb3c098cfac4476469290cb
Mxtt-smith <117466868+Mxtt-smith@users.noreply.github.com>
2024-03-12 16:15:24 +0000

    Bit of CSS on Board page

--- templates/index.js
+++
@@ -1,7 +0,0 @@
-let btn = document.getElementById("search")
-let input = document.getElementById("stationBox")
-btn.addEventListener("click", search(input.textContent))
-
-function search(s){
- url = 
-}
